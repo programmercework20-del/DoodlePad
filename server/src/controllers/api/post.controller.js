ff5640d94601dd29be204e14e62eb8e95891c337
Avesh Khan <aveshkhan9359@gmail.com>
2026-02-04 13:15:30 +0530

    delete post done

--- server/src/controllers/api/post.controller.js
+++ server/src/controllers/api/post.controller.js
@@ -51,3 +51,52 @@ export const createPost = async (req, res) => {
     res.status(500).json({ success: false, message: "Failed to create post" });
   }
 };
+
+export const deletePost = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const postId = req.params.id;
+
+    // 1. Find Post
+    const post = await Post.findByPk(postId);
+
+    if(!post){
+      return res.status(404).json({
+        success: false,
+        message: "Post not found"
+      });
+    }
+
+    // 2.check ownership
+
+    if(post.userId !== userId){
+      return res.json(403).json({
+        success: false,
+        message: "you are not allowed to delete this post"
+      });
+    }
+
+    // 2. Already deleted?
+
+    if(post.userId === "deleted"){
+      return res.json(400).json({
+        success: false,
+        message: "post already deleted"
+      });
+    }
+
+    await post.update({status: "deleted"});
+
+    return res.json({
+      success: true,
+      message: "Post deleted successfully"
+    });
+    
+  } catch (error) {
+     console.error("Delete post error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to delete post"
+    });
+  }
+}
