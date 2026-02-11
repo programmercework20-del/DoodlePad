55fb00a4911ea003011dd4865c3bdd16f143f6ed
programmercework20-del <programmercework20@gmail.com>
2026-02-11 20:03:37 +0530

    duplicate entry in app.js 6

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,22 +1,25 @@
-import { Sequelize } from 'sequelize';
-import config from './env.js';
+import { Sequelize } from "sequelize";
+import config from "./env.js";
+
+console.log("🔥 DB CONFIG FILE LOADED");
 
 const sequelize = new Sequelize(
-    config.db.name,
-    config.db.user,
-    config.db.password,
-    {
-        host: config.db.host,
-        port: config.db.port,
-        dialect: config.db.dialect,
-        logging: config.nodeEnv === "development" ? console.log : false,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-    }
+  config.db.name,
+  config.db.user,
+  config.db.password,
+  {
+    host: config.db.host,
+    port: config.db.port,
+    dialect: "postgres",
+    logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // VERY IMPORTANT FOR AWS RDS
+      },
+    },
+  }
 );
 
 export default sequelize;
