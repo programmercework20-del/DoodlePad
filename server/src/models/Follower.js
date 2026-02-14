e979f8c8099ac6a796704f07755925fa3e574676
Avesh Khan <aveshkhan9359@gmail.com>
2026-02-14 15:29:04 +0530

    changes done

--- server/src/models/Follower.js
+++ server/src/models/Follower.js
@@ -1,30 +1,32 @@
-import { DataTypes } from "sequelize";
-import sequelize from "../config/db.js";
+export default (sequelize, DataTypes) => {
+  const Follower = sequelize.define(
+    "Follower",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
 
-const Follower = sequelize.define("Follower", {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true
-  },
+      followerId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: "followerId",   // 👈 IMPORTANT
+      },
 
-  followerId: {
-    type: DataTypes.UUID,
-    allowNull: false,
-    field: "follower_id"   // 🔥 map DB column
-  },
+      followingId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: "followingId",  // 👈 IMPORTANT
+      },
+    },
+    {
+      tableName: "followers",
+      timestamps: true,
+      createdAt: "createdAt",
+      updatedAt: false, // you don’t have updatedAt column
+    }
+  );
 
-  followingId: {
-    type: DataTypes.UUID,
-    allowNull: false,
-    field: "following_id"  // 🔥 map DB column
-  }
-
-},{
-  tableName:"followers",
-  timestamps:true,
-  createdAt:"created_at",
-  updatedAt:false
-});
-
-export default Follower;
+  return Follower;
+};
