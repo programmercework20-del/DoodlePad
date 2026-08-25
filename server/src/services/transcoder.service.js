824230816944e762bd300cc6f6cbdd946a134dd6
Programmerce Tech <programmercework20@gmail.com>
2026-08-25 19:50:50 +0530

    enable cdn bucket same as

--- server/src/services/transcoder.service.js
+++ server/src/services/transcoder.service.js
@@ -7,7 +7,8 @@ const RAW_BUCKET_NAME = 'doodlepad-media-staging';
 
 // Humara naya HLS bucket
 // const HLS_BUCKET_NAME = 'doodlepad-hls-output'; 
-const HLS_BUCKET_NAME = 'doodlepad-cdn-bucket';
+// const HLS_BUCKET_NAME = 'doodlepad-cdn-bucket';
+const HLS_BUCKET_NAME = 'doodlepad-media-staging';
 
 // URL se nikala hua exact Project ID
 const projectId = 'project-7531567b-e7c3-4c4e-8fe'; 
